aaadf413410f71d591496f71ccb86d6b0a79158f
妖刀 <2547567573@qq.com>
2025-05-13 18:08:51 +0800

    完善页面；创建文件处理模块

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 slint::include_modules!();
 
+pub mod operation_file;
+
 fn main() {
     let main_window = MainWindow::new().unwrap();
+
+    main_window.on_login(move || {
+        println!("登录逻辑");
+    });
+
+    let file_name = operation_file::add(1, 2);
+    println!("{}", file_name);
 
     main_window.run().unwrap();
 }
